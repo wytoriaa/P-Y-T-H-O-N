695a19db5cc28402e7441536036dfb21ed1aa4c3
Wytoria Faustino <86625354+wytoriaa@users.noreply.github.com>
2021-12-18 01:23:00 -0300

    calculando algumas areas

---
+++ area.py
@@ -0,0 +1,19 @@
+#calculando a area de um triangulo retangulo
+a, b, c = input().split()
+a = float(a)
+b = float(b)
+c = float(c)
+pi = 3.14159
+
+triangulo =( a * c )/2
+circulo = pi *(c ** 2)
+trapezio = ((a + b) * c)/2
+quadrado = b * b
+retangulo = a * b
+#######################################
+print("TRIANGULO: {:.3f}".format(triangulo))
+print("CIRCULO: {:.3f}".format(circulo))
+print("TRAPEZIO: {:.3f}".format(trapezio))
+print("QUADRADO: {:.3f}".format(quadrado))
+print("RETANGULO: {:.3f}".format(retangulo))
+
