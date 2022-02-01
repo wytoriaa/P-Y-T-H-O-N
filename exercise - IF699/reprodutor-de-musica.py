f921f0f2182b1ac1526c0d5a803a0a787661ffd3
Wytoria Faustino <86625354+wytoriaa@users.noreply.github.com>
2022-02-01 08:44:19 -0300

    exercicio

---
+++ exercise - IF699/reprodutor-de-musica.py
@@ -0,0 +1,13 @@
+#se você digitasse uma palavra-chave em relação à música, você selecionaria ela
+
+keyword = input()
+if keyword == 'amor':
+  print(f'Voce selecionou a musica: SE FOR AMOR - Joao Gomes e Vitor')
+elif keyword == 'ficha':
+  print(f'Voce selecionou a musica: FICHA LIMPA - Gusttavo Lima')
+elif keyword == 'chega':
+  print(f'Voce selecionou a musica: CHEGA E SENTA - John')
+elif keyword == 'arranhao':
+  print(f'Voce selecionou a musica: ARRANHAO - Henrique e Juliano')
+else:
+  print(f'Voce selecionou a musica: VOLTA COMIGO BB - Ze Vaqueiro')
