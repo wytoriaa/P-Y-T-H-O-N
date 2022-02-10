ced5f521a28e2455c1bfbc8f3414059b4fdb4307
Wytoria Faustino <86625354+wytoriaa@users.noreply.github.com>
2022-02-10 11:57:38 -0300

    escolhas

---
+++ exercise - IF699/escolhas.py
@@ -0,0 +1,8 @@
+p = input()
+
+if p == 'Pilula azul':
+  print('Você acordará na sua cama e acreditará no que quiser acreditar. Lembra-se que tudo o que te ofereço é a verdade. Nada mais.')
+elif p == 'Pilula vermelha':
+  print('Boas vindas ao País das Maravilhas, você verá com os próprios olhos até quão fundo vai o buraco do coelho.')
+else:
+  print('Algo aconteceu com a comunicação, precisamos voltar imediatamente. Enquanto a você, caso ainda haja algo como uma farpa na tua mente, nos veremos em outro momento.')
