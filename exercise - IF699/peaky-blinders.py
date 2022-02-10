b0fba3509f3dbdf092d5c0eb8d331ecb8d87a906
Wytoria Faustino <86625354+wytoriaa@users.noreply.github.com>
2022-02-10 11:58:26 -0300

    peaky blinders

---
+++ exercise - IF699/peaky-blinders.py
@@ -0,0 +1,22 @@
+arma = int(input())
+
+if arma == 1:
+  print(f'A arma corpo a corpo escolhida foi: Cassetete')
+  print('A arma corpo a corpo escolhida e atordoante')
+elif arma == 2:
+  print(f'A arma corpo a corpo escolhida foi: Garrafa de Whisky')
+  print('A arma corpo a corpo escolhida e cortante')
+elif arma == 3:
+  print(f'A arma corpo a corpo escolhida foi: Soco Ingles')
+  print('A arma corpo a corpo escolhida e atordoante')
+elif arma == 4:
+  print(f'A arma corpo a corpo escolhida foi: Lamina Escondida')
+  print('A arma corpo a corpo escolhida e cortante')
+elif arma == 5:
+  print(f'A arma corpo a corpo escolhida foi: Pe de Cabra')
+  print('A arma corpo a corpo escolhida e atordoante')
+elif arma == 6:
+  print(f'A arma corpo a corpo escolhida foi: Canivete')
+  print('A arma corpo a corpo escolhida e cortante')
+else:
+  print('Numero invalido')
